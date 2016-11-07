6dbcae42c9c09d5e5ee7e72d8d7910c3e2dce11c
xpitr256 <pablomassuh@gmail.com>
2016-11-07 13:32:22 -0300

    Paloma sobreescribe trait

--- src/traits2/Paloma.scala
+++ src/traits2/Paloma.scala
@@ -2,4 +2,5 @@ package traits2
 
 class Paloma extends Ave with Nado with Vuelo{
     val mensajeAlVolar = "Soy una buena voladora"
+    override def nadar() = println("Soy una Paloma y nado medio mal, pero nado.")
 }
